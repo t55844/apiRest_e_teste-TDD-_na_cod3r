4f4ddb50b290b7ee5aa68b2ff3002601b54941ef
thiago <thiagosoares111@yahoo.com.br>
2022-07-03 16:21:08 -0300

    Jest e Asincronismo

--- test/jest.test.js
+++ test/jest.test.js
@@ -1,4 +1,4 @@
-test('Devo conhecer as principais assertivas do jest',()=>{
+test('Devo conhecer as principais assertivas do jest', () => {
     let number = null
     expect(number).toBeNull()
     number = 10
@@ -9,8 +9,26 @@ test('Devo conhecer as principais assertivas do jest',()=>{
     expect(number).toBeLessThan(11)
 })
 
-test('Devo saber trabalhar com objetos',()=>{
-    const obj = {name:'John' ,email:'john@mail.com'}
+test('Devo saber trabalhar com objetos', () => {
+    const obj = { name: 'John', email: 'john@mail.com' }
     expect(obj).toHaveProperty('name')
-    expect(obj).toHaveProperty('name','Caio')
+})
+
+// Sincronismo no jest
+
+const supertest = require('supertest')
+
+const request = supertest('http://www.google.com')
+
+//A resposta que vira sera a 200 mas sem o return ele nao espera e continua com se desse certo
+//Mesmo os codigos sendo os mesmo sem o return ele nao funciona por que e asincrono
+
+test('Sincronismo com return faz a execução esperar  a resposta', () => {
+    return request.get('/')
+        .then(res => expect(res.status).toBe(400))// vai falhar
+})
+
+test('Sincronismo sem return nao esperar  a resposta', () => {
+    request.get('/')
+        .then(res => expect(res.status).toBe(400))// aqui nao vai 
 })
