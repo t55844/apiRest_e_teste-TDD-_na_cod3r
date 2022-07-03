763b23b631da830dd9dec829a163414eb6a18068
thiago <thiagosoares111@yahoo.com.br>
2022-07-03 17:40:45 -0300

    get('/users')
      fiz a rota que devolve os usuarios

--- src/app.js
+++ src/app.js
@@ -6,4 +6,11 @@ app.get('/', (req, res) => {
     res.status(200).send()
 })
 
+app.get('/users', (req, res) => {
+    const users = [
+        { name: 'John Doe', email: 'john@mail.com' },
+    ]
+    res.status(200).json(users)
+})
+
 module.exports = app
