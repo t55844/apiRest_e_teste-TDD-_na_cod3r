6f0433b2835b878f0b0db36683e6150fb7a74e95
thiago <thiagosoares111@yahoo.com.br>
2022-07-03 18:07:20 -0300

    post('/users')
      fazendo a rota do metodo post

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.status(200).send()
@@ -11,6 +14,10 @@ app.get('/users', (req, res) => {
         { name: 'John Doe', email: 'john@mail.com' },
     ]
     res.status(200).json(users)
+})
+
+app.post('/users', (req, res) => {
+    res.status(201).json(req.body)
 })
 
 module.exports = app
--- test/user.test.js
+++ test/user.test.js
@@ -10,3 +10,12 @@ test('Deve listar todos os usuarios', () => {
             expect(res.body[0]).toHaveProperty('name', 'John Doe')
         })
 })
+
+test('Deve inserir usuarios com sucesso', () => {
+    return request(app).post('/users')
+        .send({ name: 'Virilidade Alfa', mail: 'virilidade@mail.com' })
+        .then((res) => {
+            expect(res.status).toBe(201)
+            expect(res.body.name).toBe('Virilidade Alfa')
+        })
+})
